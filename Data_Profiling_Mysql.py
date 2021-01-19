5211289c060f1e829a8e03171018667e210ba794
dhmec <dharmoradiya@gmail.com>
2021-01-18 17:48:22 -0700

    Added sql query to list all the table and schema

--- Data_Profiling_Mysql.py
+++ Data_Profiling_Mysql.py
@@ -2,6 +2,7 @@ import mysql.connector
 import pandas as pd
 import pandas_profiling as pp 
 import sys
+import json
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -11,18 +12,11 @@ mydb = mysql.connector.connect(
   database="bank_db"
 )
 mycursor = mydb.cursor()
-sql = "show index from transaction "
+sql = "SELECT table_schema, table_name FROM information_schema.tables WHERE NOT (table_schema='sys' OR table_schema='phpmyadmin' OR table_schema='performance_schema' OR table_schema='mysql' OR table_schema='information_schema')"
 #mycursor.execute(sql)
 #data = pd.DataFrame(mycursor)
 df = pd.read_sql(sql, mydb) 
+df.to_json('list_of_tables.json')
 
 print(df)
 print(len(df))
-#print(data)
-#print(mycursor.rowcount)
-#profile = pp.ProfileReport(df) 
-#profile.to_file("output.json")
-#num_fields = mycursor.description
-#print(num_fields)
-#field_names = [i[0] for i in mycursor.description]
-#print(field_names)
